5df794e15927ecac7b3941ccb6fd29198a4243fd
sebastienlorber <lorber.sebastien@gmail.com>
2023-08-31 19:02:12 +0200

    Initial Jest website Argos commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# jest-website visual-tests
+
+Tests for https://github.com/facebook/jest website
+
+To easily compare screenshots of prod vs screenshots of another deployment
+
---
+++ tests/siteScreenshots.spec.ts
@@ -0,0 +1,107 @@
+import { test } from "@playwright/test";
+import { argosScreenshot } from "@argos-ci/playwright";
+import * as fs from "fs";
+import * as cheerio from "cheerio";
+
+const siteUrl = process.env.SITE_URL ?? "https://jestjs.io/";
+const isProd = siteUrl === "https://jestjs.io/";
+
+function extractSitemapUrls() {
+  const sitemapString = fs.readFileSync("./sitemap.xml") as any;
+  const $ = cheerio.load(sitemapString, { xmlMode: true });
+  const urls: string[] = [];
+  $("loc").each(function () {
+    urls.push($(this).text());
+  });
+  return urls;
+}
+
+const BlacklistedPathnames: string[] = [
+  // TODO
+];
+
+function isBlacklisted(pathname: string) {
+  return (
+    // skip versioned docs
+    pathname.match(/^\/docs\/(\d\.\d\d)|(next)\//) ||
+    // manually excluded urls
+    BlacklistedPathnames.includes(pathname)
+  );
+}
+
+const getPathnames = function (): string[] {
+  const urls = extractSitemapUrls();
+  const pathnamesUnfiltered = urls.map((url) => new URL(url).pathname);
+  const pathnames = pathnamesUnfiltered.filter(
+    (pathname) => !isBlacklisted(pathname)
+  );
+  pathnames.sort();
+  console.log("Pathnames:\n", JSON.stringify(pathnames, null, 2));
+  console.log("Pathnames before filtering", pathnamesUnfiltered.length);
+  console.log("Pathnames after filtering", pathnames.length);
+  return pathnames;
+};
+
+// Hide elements that may vary between prod/preview
+/* language=css */
+const stylesheet = `
+/* Global + Docusaurus theme flaky elements */
+iframe, 
+.avatar__photo,
+.DocSearch-Button-Keys,
+img[src$=".gif"] {
+  visibility: hidden;
+}
+
+.theme-last-updated {
+  display: none;
+}
+
+/* TODO Jest Website flaky elements */
+
+
+
+`;
+
+function pathnameToArgosName(pathname: string): string {
+  function removeTrailingSlash(str: string): string {
+    return str.endsWith("/") ? str.slice(0, -1) : str;
+  }
+  function removeLeadingSlash(str: string): string {
+    return str.startsWith("/") ? str.slice(1) : str;
+  }
+
+  pathname = removeTrailingSlash(pathname);
+  pathname = removeLeadingSlash(pathname);
+
+  if (pathname === "") {
+    return "index";
+  }
+
+  return pathname;
+}
+
+// See https://github.com/facebook/docusaurus/pull/9256
+// Docusaurus adds a <html data-has-hydrated="true">
+function waitForDocusaurusHydration() {
+  return document.documentElement.dataset.hasHydrated === "true";
+}
+
+function createPathnameTest(pathname: string) {
+  test(`pathname ${pathname}`, async ({ page }) => {
+    const url = siteUrl + pathname;
+    await page.goto(url);
+    if (!isProd) {
+      await page.waitForFunction(waitForDocusaurusHydration);
+    }
+    await page.evaluate(async () => new Promise((r) => requestIdleCallback(r)));
+    await page.addStyleTag({ content: stylesheet });
+    await argosScreenshot(page, pathnameToArgosName(pathname));
+  });
+}
+
+test.describe("site screenshots", () => {
+  const pathnames = getPathnames();
+
+  pathnames.forEach(createPathnameTest);
+});
